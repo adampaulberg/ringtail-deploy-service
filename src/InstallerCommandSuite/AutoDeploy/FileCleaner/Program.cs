fe611da725f2e4fd7189fd95988e633b0a96ead4
Brian Mancini <bmancini@gmail.com>
2015-04-06 15:24:35 -0400

    Added file deletion helper

---
+++ src/InstallerCommandSuite/AutoDeploy/FileCleaner/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace FileCleaner
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {               
+            var options = new Options();
+            if(!CommandLine.Parser.Default.ParseArguments(args, options))
+            {                
+                return 1;
+            }
+
+            var cleaner = new CleanerHelper(options);            
+            var exitCode = cleaner.Process();
+            Console.WriteLine(cleaner.WriteLog());
+            cleaner.WriteOutput();
+
+            Console.WriteLine("Exiting with code " + exitCode);
+            return exitCode;
+        }
+    }    
+}
