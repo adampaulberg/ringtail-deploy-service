4e6412a0b1db106a981e286542fa8a700fcfe698
Brian Mancini <bmancini@gmail.com>
2015-05-05 11:13:49 -0400

    Included API Logging code that was missing

---
+++ src/InstallerCommandSuite/InstallerService/InstallerService/ApiLogger.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+
+namespace InstallerService
+{
+    /// <summary>
+    /// Logger task
+    /// </summary>
+    public class ApiLogger
+    {
+        public ApiLogger(string path)
+        {
+            this.Path = path;
+
+            this.WriteQueue = new ConcurrentQueue<string>();
+            this.WatchQueueTask = new Task(WatchQueue);
+            this.WatchQueueTask.Start();
+
+            Log("Starting logger");
+        }
+
+        public string Path { get; set; }
+
+        Task WatchQueueTask { get; set; }
+        ConcurrentQueue<string> WriteQueue { get; set; }
+
+        public void Log(string data)
+        {
+            var logdata = string.Format("{0} - {1}", GetDateString(), data);
+            WriteQueue.Enqueue(logdata);
+        }
+
+        public void Log(HttpRequestMessage request)
+        {
+            var logdata = string.Format("{0} - Beg: {1} {2}", GetDateString(), request.Method, request.RequestUri);
+            WriteQueue.Enqueue(logdata);
+        }
+
+        public void Log(HttpRequestMessage request, HttpResponseMessage response)
+        {
+            var logdata = string.Format("{0} - End: {1} {2} - {3}", GetDateString(), request.Method, request.RequestUri, (int)response.StatusCode);
+            WriteQueue.Enqueue(logdata);
+        }
+
+        string GetDateString()
+        {
+            return (DateTime.Now).ToString("yyyy-MM-dd HH:mm:ss.fff");
+        }
+
+        void WatchQueue()
+        {
+            while (true)
+            {
+                string logdata;
+                if (WriteQueue.TryDequeue(out logdata))
+                {
+                    WriteLogData(logdata);
+                }
+                System.Threading.Thread.Sleep(100);
+            }
+        }
+
+        void WriteLogData(string logdata)
+        {
+            try
+            {
+                using (var fs = new FileStream(this.Path, FileMode.Append))
+                using (var sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine(logdata);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+
+    }
+}
---
+++ src/InstallerCommandSuite/InstallerService/InstallerService/ApiLoggerActionFilterAttribute.cs
@@ -0,0 +1,36 @@
+﻿using InstallerService.Daemon.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+
+namespace InstallerService
+{
+    public class ApiLoggerActionFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            var controller = actionContext.ControllerContext.Controller as BaseController;
+            if (controller != null)
+            {
+                var request = actionContext.Request;
+                controller.Logger.Log(request);
+            }
+            base.OnActionExecuting(actionContext);
+        }
+
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            var controller = actionExecutedContext.ActionContext.ControllerContext.Controller as BaseController;
+            if (controller != null)
+            {
+                var request = actionExecutedContext.Request;
+                var response = actionExecutedContext.Response;
+                controller.Logger.Log(request, response);
+            }
+            base.OnActionExecuted(actionExecutedContext);
+        }
+    }
+}
