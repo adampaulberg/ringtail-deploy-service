6d67c63f75bacc3c6599904fe9ab26dc681c00ef
Brian Mancini <bmancini@gmail.com>
2015-05-08 11:50:02 -0400

    Reorganized into a single solution

--- src/Shared/VersionInfo.cs
+++ src/Shared/VersionInfo.cs
@@ -9,5 +9,5 @@ using System.Runtime.InteropServices;
 //      Bug fix
 //      0
 //
-[assembly: AssemblyVersion("2.2.3.0")]
-[assembly: AssemblyFileVersion("2.2.3.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
