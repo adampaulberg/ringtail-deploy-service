2c90fd81c2b7fd5a4b0213dba80b39d869d80b67
jakebrown58 <jake.brown.pa@gmail.com>
2016-05-10 14:27:11 -0400

    LaunchKeysAPI - Adds local copy
    Locally copies the drop folder location to a test folder, then calls it
    from there.

--- src/InstallerService/InstallerService/Daemon/Controllers/AvailableFeaturesController.cs
+++ src/InstallerService/InstallerService/Daemon/Controllers/AvailableFeaturesController.cs
@@ -25,77 +25,84 @@ namespace InstallerService.Daemon.Controllers
 
 
             // needs to call the exe at the drop location and get the possible keys.
-            FileInfo fi = new FileInfo(EnvironmentInfo.CONFIG_LOCATION);
-            if (fi.Exists)
+            try
             {
-                try
+                if (config.ContainsKey(EnvironmentInfo.KeyMasterRunnerUser) && config.ContainsKey(EnvironmentInfo.KeyMasterRunnerPass))
                 {
-                    if (config.ContainsKey(EnvironmentInfo.KeyMasterRunnerUser) && config.ContainsKey(EnvironmentInfo.KeyMasterRunnerPass))
-                    {
-                        username = config[EnvironmentInfo.KeyMasterRunnerUser];
-                        password = config[EnvironmentInfo.KeyMasterRunnerPass];
-                    }
-
-                    log.Add("Starting.... " + DateTime.Now.ToLongDateString());
-
-                    fileName = dropLocation + "RingtailFeatureUtility.exe";
-                    string cmd = "/c " + fileName;
-                    var processInfo = new ProcessStartInfo("cmd.exe", cmd);
-
-                    var process = new System.Diagnostics.Process();
-                    process.StartInfo.FileName = fileName;
-                    process.StartInfo.WorkingDirectory = dropLocation;
-                    process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
-                    process.StartInfo.RedirectStandardOutput = true;
-                    process.StartInfo.RedirectStandardError = true;
-                    process.StartInfo.UseShellExecute = false;
-                    process.StartInfo.Arguments = "-g";
-
-                    log.Add(" start info...");
-                    log.Add(" WorkingDir: " + dropLocation);
-                    log.Add(" FileName: " + fileName);
-                    log.Add(" Cmd: " + cmd);
-
-
-                    log.Add("Cmd target: " + process.StartInfo.WorkingDirectory);
-                    log.Add("Cmd file: " + process.StartInfo.FileName);
-
-                    process.Start();
-                    string output = process.StandardOutput.ReadToEnd();
-                    string error = process.StandardError.ReadToEnd();
-
-                    int exitCode = process.ExitCode;
-
-                    if (exitCode != 0)
-                    {
-                        FileHelpers.SimpleFileWriter.Write(@"C:\Upgrade\InstallerService\availableFeaturesLog-Error.txt", log);
-                    }
-
-
-                    log.Add("Cmd errors: " + error);
-                    log.Add("Cmd output: " + output);
-                    log.Add("Cmd is: " + cmd);
-                    log.Add("Cmd exitCode: " + exitCode);
-
-                   FileHelpers.SimpleFileWriter.Write(@"C:\Upgrade\InstallerService\availableFeaturesLog.txt", log);
-
-
-                    if(exitCode == 0)
-                    {
-                        return output;
-                    }
-
+                    username = config[EnvironmentInfo.KeyMasterRunnerUser];
+                    password = config[EnvironmentInfo.KeyMasterRunnerPass];
                 }
-                catch(Exception ex)
+
+                log.Add("Starting.... " + DateTime.Now.ToLongDateString());
+
+                CopyFilesLocally(dropLocation);
+
+                fileName = @"C:\Upgrade\InstallerService\Test\" + "RingtailFeatureUtility.exe";
+                string cmd = "/c " + fileName;
+                var processInfo = new ProcessStartInfo("cmd.exe", cmd);
+
+
+                var process = new System.Diagnostics.Process();
+                process.StartInfo.FileName = fileName;
+                process.StartInfo.WorkingDirectory = dropLocation;
+                process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.Arguments = "-g";
+
+                log.Add(" start info...");
+                log.Add(" WorkingDir: " + dropLocation);
+                log.Add(" FileName: " + fileName);
+                log.Add(" Cmd: " + cmd);
+
+
+                log.Add("Cmd target: " + process.StartInfo.WorkingDirectory);
+                log.Add("Cmd file: " + process.StartInfo.FileName);
+
+                process.Start();
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+
+                int exitCode = process.ExitCode;
+
+                if (exitCode != 0)
                 {
-                    log.Add(ex.Message);
-                    log.Add(ex.StackTrace);
-                    FileHelpers.SimpleFileWriter.Write(@"C:\Upgrade\InstallerService\availableFeaturesLogError.txt", log);
-                    return "ERROR: " + ex.Message;
+                    FileHelpers.SimpleFileWriter.Write(@"C:\Upgrade\InstallerService\availableFeaturesLog-Error.txt", log);
                 }
+
+                log.Add("Cmd errors: " + error);
+                log.Add("Cmd output: " + output);
+                log.Add("Cmd is: " + cmd);
+                log.Add("Cmd exitCode: " + exitCode);
+
+                FileHelpers.SimpleFileWriter.Write(@"C:\Upgrade\InstallerService\availableFeaturesLog.txt", log);
+
+
+                if (exitCode == 0)
+                {
+                    keys = output;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                log.Add(ex.Message);
+                log.Add(ex.StackTrace);
+                FileHelpers.SimpleFileWriter.Write(@"C:\Upgrade\InstallerService\availableFeaturesLogError.txt", log);
+                return "ERROR: " + ex.Message;
             }
 
             return keys;
+        }
+
+        private static void CopyFilesLocally(string dropLocation)
+        {
+            var fi = new FileInfo(dropLocation + "RingtailFeatureUtility.exe");
+            fi.CopyTo(@"C:\upgrade\InstallerService\test\RingtailFeatureUtility.exe", true);
+
+            fi = new FileInfo(dropLocation + "RingtailDarkKeys.csv");
+            fi.CopyTo(@"C:\upgrade\InstallerService\test\RingtailDarkKeys.csv", true);
         }
     }
 }
