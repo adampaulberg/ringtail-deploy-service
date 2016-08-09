6d1e4d88a830b3c9607713e526594ef4c5e527e6
jakebrown58 <jake.brown.pa@gmail.com>
2016-08-09 13:32:28 -0400

    Adding support for zip file installs
    Lets you do installs that are completed via a powershell script running
    against a zip file.  Rather than an install shield installer.

---
+++ src/AutoDeploy/ServiceFetcher/App/Options.cs
@@ -0,0 +1,47 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceFetcher.App
+{
+    public class Options
+    {
+        public string Address { get; set; }
+
+        [Option('f', "root", Required = true, HelpText = "Folder Root")]
+        public string FolderRoot { get; set; }
+
+        [Option('b', "branch", HelpText = "Branch")]
+        public string BranchName { get; set; }
+
+        [Option('s', "suffix", HelpText = "Folder Suffix", DefaultValue="Microservices")]
+        public string FolderSuffix { get; set; }
+
+        [Option('a', "appName", HelpText = "Application Name")]
+        public string ApplicationName { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "Output file suffix")]
+        public string Output { get; set; }
+
+        [Option('e', "errorLevel", Required = false, DefaultValue = "1", HelpText = "Error level.")]
+        public string ErrorLevel { get; set; }
+
+        internal int GetErrorLevel()
+        {
+            int val = 0;
+            Int32.TryParse(ErrorLevel, out val);
+            return val;
+        }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
---
+++ src/AutoDeploy/ServiceFetcher/Util/Util.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceFetcher.Util
+{
+    public class SimpleFileWriter
+    {
+        public static void Write(string fileName, List<string> s)
+        {
+            using (StreamWriter wr = new StreamWriter(fileName))
+            {
+                foreach (string str in s)
+                {
+                    wr.WriteLine(str);
+                }
+            }
+        }
+    }
+
+    public class SimpleFileReader
+    {
+        public static List<string> Read(string fileName)
+        {
+            List<string> s = new List<string>();
+
+            FileInfo fi = new FileInfo(fileName);
+            if (fi.Exists)
+            {
+
+                using (StreamReader stream = new StreamReader(fileName))
+                {
+                    string input = null;
+                    while ((input = stream.ReadLine()) != null)
+                    {
+                        s.Add(input);
+                    }
+                }
+            }
+
+            return s;
+        }
+    }
+
+
+    public static class DirectoryInfoExtensions
+    {
+        /// <summary>
+        /// Checks whether the folder exists and has files.
+        /// </summary>
+        /// <param name="folder"></param>
+        /// <returns></returns>
+        public static bool HasFiles(this DirectoryInfo folder)
+        {
+            return folder.Exists ? folder.GetFiles().Length != 0 : false;
+        }
+
+        /// <summary>
+        /// Returns the folder, or if passed in a suffix, the subfolder with that name.
+        /// </summary>
+        /// <param name="folder"></param>
+        /// <param name="suffix">optional param.  send in null if you want to repeat the folder back to you.</param>
+        /// <returns></returns>
+        public static DirectoryInfo NormalizeFolderBySuffix(this DirectoryInfo folder, string suffix)
+        {
+            DirectoryInfo working = folder;
+            if (!String.IsNullOrEmpty(suffix))
+            {
+                working = new DirectoryInfo(folder.FullName + @"\" + suffix);
+            }
+
+            return working;
+        }
+    }
+
+    public static class ListExtensions
+    {
+        public static void AppendToFront<T>(this List<T> list, T item)
+        {
+            list.Reverse();
+            list.Add(item);
+            list.Reverse();
+        }
+    }
+
+}
