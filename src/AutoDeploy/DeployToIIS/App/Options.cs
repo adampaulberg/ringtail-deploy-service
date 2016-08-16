1738418e18b4dde012be583e18e06521d890a307
jakebrown58 <jake.brown.pa@gmail.com>
2016-08-16 14:38:00 -0400

    Adding an IIS deployer tool for IIS running services

---
+++ src/AutoDeploy/DeployToIIS/App/Options.cs
@@ -0,0 +1,36 @@
+﻿
+using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TestIISDeploy
+{
+    public class Options
+    {
+        [Option('u', "username", Required = true, HelpText = "username")]
+        public string Username { get; set; }
+
+        [Option('p', "password", Required = true, HelpText = "password")]
+        public string Password { get; set; }
+
+        [Option('a', "appname", Required = true , HelpText = "The name of the application")]
+        public string AppName { get; set; }
+
+        [Option('i', "install-path", Required = true, HelpText = "Path of the service")]
+        public string InstallPath { get; set; }
+
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
